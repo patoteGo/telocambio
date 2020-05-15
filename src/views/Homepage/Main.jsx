367080b5f6a37c755c86af1e197beed75914e5a9
patodekaz <pato@dekaz.cl>
2020-05-14 22:48:47 -0400

    add card to main

--- src/views/Homepage/Main.jsx
+++ src/views/Homepage/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Card from './components/Card'
 function Main() {
   return (
     <div className="Homepage-main">
       MAIN
+      <Card/>
     </div>
   );
 }
---
+++ src/views/Homepage/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Card() {
+  return (
+    <div className="Homepage-main">
+      Card
+      
+    </div>
+  );
+}
+
+export default Card;
