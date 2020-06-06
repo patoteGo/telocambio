331dce2397c54b666fef548646438f2eebf70440
patodekaz <pato@dekaz.cl>
2020-06-06 08:47:41 -0400

    change name of the menu

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -14,22 +14,28 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarsExample07">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">INICIO <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">PRODUCTOS</a>
+                <a className="nav-link" href="#">Productos</a>
               </li>
-              <li className="nav-item dropdown">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Quienes somos</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contáctanos</a>
+              </li>
+              {/* <li className="nav-item dropdown">
                 <div className="nav-link dropdown-toggle"  id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</div>
                 <div className="dropdown-menu" aria-labelledby="dropdown07">
                   <a className="dropdown-item" href="#">Action</a>
                   <a className="dropdown-item" href="#">Another action</a>
                   <a className="dropdown-item" href="#">Something else here</a>
                 </div>
-              </li>
+              </li> */}
             </ul>
             <form className="form-inline my-2 my-md-0" _lpchecked="1">
-              <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
+              <input className="form-control" type="text" placeholder="Buscar.." aria-label="Buscar.."/>
             </form>
           </div>
         </div>
