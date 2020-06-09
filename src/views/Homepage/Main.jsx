b8e380952ccbf14a8b1ae99b16e11a99d804047a
anita <anitamaria.cor@gmail.com>
2020-06-08 20:31:18 -0400

    cardavance

--- src/views/Homepage/Main.jsx
+++ src/views/Homepage/Main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from './components/Card'
+
 function Main() {
   return (
-    <div className="Homepage-main">
-      MAIN
-  <hola>hola</hola>
-
-      <Card hijo="Anita" name="card1" img="https://picsum.photos/200/300?random=1"/>
-      <Card hijo="Guillermo" name="card2" img="https://picsum.photos/200/300?random=2"/>
-      <Card hijo="Pato" name="card3" img="https://picsum.photos/200/300?random=3"/>
+    <div className="Main">
+      <Card/>
+      <Card/>
+      <Card/>
     </div>
   );
 }
--- src/views/Homepage/components/Card.jsx
+++ src/views/Homepage/components/Card.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CardChild from './CardChild.jsx';
-function Card({name, img, hijo}) {
-  return (
-    <div className="Homepage-main">
-      {name}
-      <img src={img} alt=""/>
-      <CardChild hijo={hijo}/>
-    </div>
-  );
-}
 
-// function Card(props) {
-//   return (
-//     <div className="Homepage-main">
-//       {props.name}
-//       <img src={props.img} alt=""/>
-      
-//     </div>
-//   );
-// }
+
+function Card() {
+  return(
+   <div content>
+    <div className="card" style={{width: "18rem"}}>  
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQBcyk8gCoEl_YGpt-lAV23d6cvD3MUsQ6TGGKK-D3JuyXxnxnu&usqp=CAU" className="card-img-top" alt="..."></img>
+    </div>
+    <div className="card-body">
+      <h5 className="card-title">Juguera</h5>
+      <p className="card-text">Juguera en buen estado, cambio o vendo por no uso.</p>
+    </div>
+   </div>
+  )
+}
 
 export default Card;
