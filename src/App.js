142356ec050c6a1d1edb6bda0d50fe1e1a34c202
anita <anitamaria.cor@gmail.com>
2020-06-23 15:54:49 -0400

    listo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './middleware/Routes'
 import { AppProvider } from './Context/AppContext.jsx'
 import './sass/_fonts.sass'
-import Contacto from './views/Contacto'
+
 function App() { 
   return (
     <AppProvider>
     <div className="App">
       <Routes/>
-      <Contacto/>
+     
     </div>
     </AppProvider>
   );
