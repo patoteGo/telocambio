d2009051ccdbce77b152157163dcdec3e8f9d4fe
anita <anitamaria.cor@gmail.com>
2020-06-23 21:12:58 -0400

    titulo

---
+++ src/views/Quienes-somos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './../layouts/Header'
+import Footer from './../layouts/footer/Footer2'
+
+export default function Contacto(){
+    return(
+        <div className = "container">
+            <h1>Quienes Somos</h1>
+        </div>
+
+    )
+}
