fe7b0d9b5d4b78c589076779721cee8970715ecc
anita <anitamaria.cor@gmail.com>
2020-06-24 22:19:53 -0400

    margin

--- src/views/QuienesSomos.jsx
+++ src/views/QuienesSomos.jsx
@@ -8,11 +8,13 @@ export default function Contacto() {
         <div>
             <div>
                 <Header />
-                <div class="container-fluid">
+                <div class="container">
+                    <div class= "row">
                     <h1>Quienes Somos</h1>
                     <p>Telocambio nace de la necesidad de generar más canales de intercambio que no dependan del dinero, para que las personas puedan adquirir productos que les son útiles sin desembolsar ni un peso, esto se lleva a cabo ofreciendo cosas que ya no se ocupan o simplemente podrían ir al abandono, se les da otra vida siendo reutilizadas por otra persona.
                     El intercambio o trueque es un acto de resistencia al uso obligatorio del dinero como medio para obtener productos o servicios. Con el trueque podemos nosotros mismos dar valor a las cosas, según nuestras expectativas rigiéndonos también por calidad, cantidad, tiempo de uso, etc.
                     </p>
+                    </div>
                 </div>
                 <Footer />
             </div >
