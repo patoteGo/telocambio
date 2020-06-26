34e3f20b68a06160a43afb149445965b536e7741
guillermosequera <sequeracrespo@gmail.com>
2020-06-26 12:00:03 -0400

    enviocorreo

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
+import { SendEmail } from '../config/api.js'
 import {useForm} from 'react-hook-form';
 import Header from './../layouts/Header'
 import Footer from './../layouts/footer/Footer2'
@@ -9,8 +10,9 @@ export default function Contacto() {
     const {register, handleSubmit, errors} = useForm();
 
     const onSubmit = (data) => {
-        console.log(data);
-    };
+        console.log(data)
+        SendEmail(data)
+    }
 
     return (
         <div>
@@ -24,22 +26,24 @@ export default function Contacto() {
                                     <h3 className="text-center text-info">Contacto</h3>
                                     <div className="form-group">
                                         <label htmlFor="username" className="text-info">Nombre:</label><br />
-                                        <input type="text" name="username" id="username" className="form-control" ref={register({required: true, minLength: 3})}/>
+                                        <input type="text" name="username" id="username" className="form-control" /*ref={register({required: true, minLength: 3})}*//>
                                         {errors.username === "minLength" && (<p>Minimo 3 caracteres</p>)}
                                         {errors.username === "required" && (<p>Este campo es obligartorio</p>)}
-                                        {errors.username === "pattern" && (<p>Solo letras mayusculas y minusculas</p>)}
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="lastname" className="text-info">Apellido:</label><br />
-                                        <input type="text" name="lastname" id="lastname" className="form-control" ref={register({required: true})}/>
+                                        <input type="text" name="subject" id="subject" className="form-control" ref={register({required: true})}/>
+                                        {errors.lastname === "required" && (<p>Este campo es obligartorio</p>)}
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="email" className="text-info">Email:</label><br />
-                                        <input type="text" name="email" id="email" className="form-control" />
+                                        <input type="text" name="email" id="email" className="form-control" 
+                                        ref={register({required: true})}
+                                        />
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="message" className="text-info">Mensaje:</label><br />
-                                        <input type="text" name="message" id="message" className="form-control" />
+                                        <input type="text" name="body" id="body" className="form-control" ref={register({required: true})} />
                                     </div>
                                     <div className="form-group">
                                         <input type="submit" name="submit" className="btn btn-info btn-md" value="Enviar" />
