df00e5fb9630a9cec4650c8cec279c03d1d92b93
patodekaz <pato@dekaz.cl>
2020-08-05 17:49:33 -0400

    fix header

--- src/modals/closeModal.js
+++ src/modals/closeModal.js
@@ -2,7 +2,10 @@ const closeModal = (modal) => {
     document.querySelector('body').classList.remove('modal-open')
     document.querySelector('body').style.paddingRight = '0px'
     document.querySelector(`#${modal}`).classList.remove('show')
-    document.querySelector('.modal-backdrop').classList.remove('show')
+    // document.querySelector('.modal-backdrop').classList.remove('show')
+    //new
+    document.querySelector(`#${modal}`).style.display = 'none';
+    document.querySelector('.modal-backdrop').remove()
 }
 
 export default closeModal
